7b5ef08536de74024c500de96f01719c34112634
preranabav <36420831+preranabav@users.noreply.github.com>
2018-02-27 00:16:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,40 +1,111 @@
 var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res){
-    res.send('article two requested and served hare');
-});
-app.get('/article-three',function(req,res){
-    res.send('article three requested and served hare');
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+ var morgan = require('morgan');
+ var path = require('path');
+ 
+ var app = express();
+ app.use(morgan('combined'));
+ 
+ 
+ var articles = {
+     'article-one': {
+       title: 'Article One | Tanmai Gopal',
+       heading: 'Article One',
+       date: 'Sep 5, 2016',
+       content: `
+           <p>
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+           </p>
+           <p>
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+           </p>
+           <p>
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+           </p>`
+     },
+     'article-two': {
+       title: 'Article Two | Tanmai Gopal',
+       heading: 'Article Two',
+       date: 'Sep 10, 2016',
+       content: `
+           <p>
+               This is the content for my second article.
+           </p>`
+     },
+     'article-three': {
+       title: 'Article Three | Tanmai Gopal',
+       heading: 'Article Three',
+       date: 'Sep 15, 2016',
+       content: `
+           <p>
+               This is the content for my third article.
+           </p>`
+     }
+ };
+ 
+ function createTemplate (data) {
+     var title = data.title;
+     var date = data.date;
+     var heading = data.heading;
+     var content = data.content;
+     
+     var htmlTemplate = `
+     <html>
+       <head>
+           <title>
+               ${title}
+           </title>
+           <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <link href="/ui/style.css" rel="stylesheet" />
+       </head> 
+       <body>
+           <div class="container">
+               <div>
+                   <a href="/">Home</a>
+               </div>
+               <hr/>
+               <h3>
+                   ${heading}
+               </h3>
+               <div>
+                   ${date}
+               </div>
+               <div>
+                 ${content}
+               </div>
+           </div>
+       </body>
+     </html>
+     `;
+     return htmlTemplate;
+ }
+ 
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+ 
++var counter = 0 ;
++app.get('/counter', function (req, res) {
++  counter = counter + 1;
++  res.send(counter.toString());
++});
++
+ app.get('/:articleName', function (req, res) {
+   // articleName == article-one
+   // articles[articleName] == {} content object for article one
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+ });
+ 
+ app.get('/ui/style.css', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+ 
+ app.get('/ui/madi.png', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
+ 
+ 
+ var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+ app.listen(8080, function () {
+   console.log(`IMAD course app listening on port ${port}!`);
+ });
