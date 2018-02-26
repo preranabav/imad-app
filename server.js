7209208dd80dff79e46f65a781451931a7f4e93b
preranabav <36420831+preranabav@users.noreply.github.com>
2018-02-27 00:47:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var express = require('express');
+ var express = require('express');
  var morgan = require('morgan');
  var path = require('path');
++var Pool = require('pg').Pool;
++
++var config = {
++    user: 'coco98',
++    database: 'coco98',
++    host: 'db.imad.hasura-app.io',
++    port: '5432',
++    password: process.env.DB_PASSWORD
++};
  
  var app = express();
  app.use(morgan('combined'));
@@ -83,12 +92,35 @@ var express = require('express');
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
  
-var counter = 0 ;
-app.get('/counter', function (req, res) {
-  counter = counter + 1;
-  res.send(counter.toString());
-});
-
++var pool = new Pool(config);
++app.get('/test-db', function (req, res) {
++   // make a select request
++   // return a response with the results
++   pool.query('SELECT * FROM test', function (err, result) {
++      if (err) {
++          res.status(500).send(err.toString());
++      } else {
++          res.send(JSON.stringify(result));
++      }
++   });
++});
++
+ var counter = 0;
+ app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+ });
+ 
+ var names = [];
+ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+   // Get the name from the request
+   var name = req.query.name;
+   
+   names.push(name);
+   // JSON: Javascript Object Notation
+   res.send(JSON.stringify(names));
+ });
+ 
  app.get('/:articleName', function (req, res) {
    // articleName == article-one
    // articles[articleName] == {} content object for article one
@@ -100,10 +132,13 @@ app.get('/counter', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
  });
  
+ app.get('/ui/main.js', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ });
+ 
  app.get('/ui/madi.png', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
  });
- 
  
  var port = 8080; // Use 8080 for local development because you might already have apache running on 80
  app.listen(8080, function () {
